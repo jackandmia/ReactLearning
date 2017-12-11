c755651e4d585a6b539a418f14d34eeefc0130be
Craig Lister <craig.lister@zuuse.com>
2017-12-11 13:45:26 +1000

    Trying and failing to add a Login event.

--- components/signinbox.jsx
+++ components/signinbox.jsx
@@ -3,6 +3,16 @@ import {Link} from 'react-router-dom';
 import './signinbox.css';
 
 export default class SignInBox extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            password: "",
+            rememberme: true
+        }
+        console.log("Loaded Box.")
+
+    }
     render() {
         return (
             <div>
@@ -10,12 +20,12 @@ export default class SignInBox extends Component {
 
                     <div className="form-signin">
                         <control-label for="inputEmail" className="sr-only">Email address</control-label>
-                        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
+                        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus onChange = {(event,newValue) => this.setState({username:newValue})} />
                         <control-label for="inputPassword" className="sr-only">Password</control-label>
-                        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+                        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required value={this.state.password} />
                         <div className="checkbox">
                             <control-label>
-                                <input type="checkbox" value="remember-me" /> Remember me
+                                <input type="checkbox" /> Remember me
                             </control-label>
                         </div>
                         <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
