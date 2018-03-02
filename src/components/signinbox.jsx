5bad48b176bffc8b581600bc03d583f31014c907
Craig Lister <craig.lister@zuuse.com>
2018-03-02 14:17:03 +1000

    Added Checkbox handler. Not working through. How do you get the value?

--- src/components/signinbox.jsx
+++ src/components/signinbox.jsx
@@ -15,11 +15,11 @@ export default class SignInBox extends Component {
         this.handleSignin = this.handleSignin.bind(this);
         this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.HandleRememberMeChange = this.HandleRememberMeChange.bind(this);
     }
 
     handleSignin(event) {
         event.preventDefault();
-
         console.log(this.state);
     }
 
@@ -28,6 +28,10 @@ export default class SignInBox extends Component {
     }
     handlePasswordChange(e) {
         this.setState( { password: e.target.value } );
+    }
+    HandleRememberMeChange(e) {
+        console.log(e.target);
+        this.setState( { rememberme: e.target.value } );
     }
 
     render() {
@@ -40,9 +44,9 @@ export default class SignInBox extends Component {
                         
                         <control-label for="inputPassword" className="sr-only">Password</control-label>
                         <input type="password" id="inputPassword" className="form-control" placeholder="Password" onChange={this.handlePasswordChange} required />
-                        <div className="checkbox">
+                        <div className="checkbox" onChange={this.HandleRememberMeChange}>
                             <control-label>
-                                <input type="checkbox" /> Remember me
+                                <input type="checkbox" value="1" /> Remember me
                             </control-label>
                         </div>
                         <button className="btn btn-lg btn-primary btn-block" type="button" onClick={this.handleSignin}>Sign in</button>
